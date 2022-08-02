3fb2354bdc44465b1475bdef155be2a50c5e04d6
Muhammad Asif <alexasifhhossain@gmail.com>
2022-08-02 09:31:09 +0600

    Read single data api

--- read_single_data.php
+++ read_single_data.php
@@ -1,31 +1,22 @@
 <?php
-// header function must use on top
+
 header('Content-Type: application/json');
 header('Acess-Control-Allow-Origin: *');
+
+include "database.php";
 
 // "php://input" -> this function Can read raw data of any format such as JSON, XML, FormData. So the request that comes inside this file will come in json format.
 // file_get_contents -> it will read the raw data from "php://input'
 // json_decode -> And we used this function to convert json to array. and we will use this array to mysqli command
 $data = json_decode(file_get_contents("php://input"), true);
+$student_id = $data['sid'];
 
-include "database.php";
-
-$sql = "SELECT * FROM rest_api";
+$sql = "SELECT * FROM rest_api WHERE id = $student_id";
 $result = mysqli_query($con, $sql) or die('SQL Query Failed');
 
 if(mysqli_num_rows($result) > 0){
     $output = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    // return as associative array format
-    //     echo '<ul>';
-    //     for($i = 0; $i <count($output); $i++){
-    //         print_r($output[$i]);
-    //         echo '<br>';
-    //     }
-    //    echo '</ul>';
-
     echo json_encode($output);
-
-
 } else{
     echo json_encode(array('message' => 'no record found.', 'status' => false));
 }
